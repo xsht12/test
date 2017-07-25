b084bb4fbfbcda424131ae4074e4e2c3aff5d354
qqsosoai <qqsosoai@github.com>
2017-07-25 17:10:54 +0800

    新的提交

--- src/test/publiaaa.java
+++ src/test/publiaaa.java
@@ -4,7 +4,12 @@ package test;
  * Created by hasee on 2017/7/25.
  */
 public class publiaaa {
+    private static Integer a=1;
+    private static Integer a1=11;
+    private static Integer a2=12;
+    private static Integer a3=13;
+    private static Integer a4=14;
     public static void main(String args[]){
-        System.out.print("aaaaa");
+        System.out.println(a);
     }
 }
